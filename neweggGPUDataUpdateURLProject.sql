49006d90f8ff00abd1f919e6d16ace8ecc0b3f0f
karterwasberg23 <karterwasberg@gmail.com>
2025-03-07 15:38:08 -0600

    Update neweggGPUDataUpdateURLProject.sql

--- neweggGPUDataUpdateURLProject.sql
+++ neweggGPUDataUpdateURLProject.sql
@@ -59,79 +59,83 @@ select availability from neweggGPUDataUpdatedURL where Availability not like 'ou
 update neweggGPUDataUpdatedURL set Availability = 'IN STOCK' where Availability not like 'out of stock' and Availability not like 'in stock';
 
 
-/*now we want to join the benchmarkGPUData table where the video card name matches the description in the neweggGPUDataUpdateURL table */
-
+/*now we want to join the benchmarkGPUData (https://www.videocardbenchmark.net/GPU_mega_page.html) table where the video card name matches the description in the neweggGPUDataUpdateURL table */
 /*create temp table for storing this joined query*/
 create table #joinedBenchmarkProduct (
 gpu_descr varchar(255), gpu_name varchar(255), 
-g3dm float, g2dm float, tdpw nvarchar(255), brand varchar(255), cprice float, prevprice float, stock varchar(255), reviews varchar(255), purl varchar(255) )
-;
+g3dm float, g2dm float, tdpw nvarchar(255), brand varchar(255), cprice float, prevprice float, stock varchar(255), reviews varchar(255), purl varchar(255) );
 
 /*use this to reset our temp table values after we adjusted our join query in testing*/
 drop table #joinedBenchmarkProduct;
-
 /* next we insert our left join query into this temp table so we can run the join query faster and filter out results
 here we are joining the benchmark data to the table with all the newegg products in it so we can see the benchmark for each product on newegg
-we also make sure to filter the out of stock items because those are not really relevant since newegg keeps old out of stock items on their site, that will never likely be restocked in the future
-*/
+we also make sure to filter the out of stock items because those are not really relevant since newegg keeps old out of stock items on their site, that will never likely be restocked in the future*/
 insert into #joinedBenchmarkProduct
 select Description, [Video Card Name],[G3D Mark], [G2D Mark], [TDP (W) ], brand, CurrentPrice, PreviousPrice, Availability, Reviews, URL from neweggGPUDataUpdatedURL left join benchmarkGPUData 
 on replace(description, ' ', '') like '%' + replace([video card name], ' ', '') + '%'
-where [Video Card Name] is not null and Availability not like 'out of stock'
-;
-
+where [Video Card Name] is not null and Availability not like 'out of stock';
 
 select * from #joinedBenchmarkProduct order by gpu_descr, gpu_name;
 /*we select from the join query and find it gives us extra results that we don't need, this is because some graphics cards have the same name but then extra editions at the end such as below
-Used - Very Good GIGABYTE GeForce GTX 1050 OC Low Profile 2GB Video Card, GBTGV-N1050OC-2GL												                    GeForce GTX 1050
-Used - Very Good GIGABYTE GeForce GTX 1050 Ti 4GB GDDR5 PCI Express 3.0 x16 ATX Video Card GV-N105TWF2OC-4GD							            GeForce GTX 1050
-Used - Very Good GIGABYTE GeForce GTX 1050 Ti 4GB GDDR5 PCI Express 3.0 x16 Low Profile Video Card GV-N105TOC-4GL						          GeForce GTX 1050
-Used - Very Good MSI GeForce GTX 1050 2GB GDDR5 PCI Express 3.0 x16 ATX Video Card GTX 1050 2G OC										                  GeForce GTX 1050
+Used - Very Good GIGABYTE GeForce GTX 1050 OC Low Profile 2GB Video Card, GBTGV-N1050OC-2GL												GeForce GTX 1050
+Used - Very Good GIGABYTE GeForce GTX 1050 Ti 4GB GDDR5 PCI Express 3.0 x16 ATX Video Card GV-N105TWF2OC-4GD							GeForce GTX 1050
+Used - Very Good GIGABYTE GeForce GTX 1050 Ti 4GB GDDR5 PCI Express 3.0 x16 Low Profile Video Card GV-N105TOC-4GL						GeForce GTX 1050
+Used - Very Good MSI GeForce GTX 1050 2GB GDDR5 PCI Express 3.0 x16 ATX Video Card GTX 1050 2G OC										GeForce GTX 1050
 Yeston GeForce GTX 1050 Ti 4GB GDDR5 Graphics cards pci express x16 3.0 video cards Desktop computer PC video gaming graphics card		GeForce GTX 1050
-Yeston GeForce GTX 1050 Ti 4GB GDDR5 LP Graphics cards pci express 3.0 video cards Desktop computer PC video gaming graphics card		  GeForce GTX 1050
-ASUS Cerberus GeForce GTX 1050 Ti 4GB OC Edition GDDR5 Gaming Graphics Card, CERBERUS-GTX1050TI-O4G										                GeForce GTX 1050 Ti
-GIGABYTE GeForce GTX 1050 Ti 4GB GDDR5 PCI Express 3.0 x16 ATX Video Cards GV-N105TD5-4GD												                      GeForce GTX 1050 Ti
+Yeston GeForce GTX 1050 Ti 4GB GDDR5 LP Graphics cards pci express 3.0 video cards Desktop computer PC video gaming graphics card		GeForce GTX 1050
+ASUS Cerberus GeForce GTX 1050 Ti 4GB OC Edition GDDR5 Gaming Graphics Card, CERBERUS-GTX1050TI-O4G										GeForce GTX 1050 Ti
+GIGABYTE GeForce GTX 1050 Ti 4GB GDDR5 PCI Express 3.0 x16 ATX Video Cards GV-N105TD5-4GD												GeForce GTX 1050 Ti
 
 as you can see the table joined gtx 1050 benchmarks on product results that were 1050 TI, but we can filter these out from the joined query
-
 if we order by gpu_descr which is from the neweggGPUDataUpdatedURL table and then by gpu_name which is from the benchmarkGPUData table length desc
-
-we can just return the first value of each row so it won't give us the results that shouldn't be joined
-*/
-
+we can just return the first value of each row so it won't give us the results that shouldn't be joined*/
 
 /*this query will now return the products we are looking for as well as the respective Benchmarks of that products however, there is still more filtering to do
 let's look at the results of this query and see if any gpu_name still joined were we didn't want them too
 */
 with rank_table as (select *, rank() over(partition by gpu_descr order by gpu_descr, len(gpu_name) desc) as GPUrank from #joinedBenchmarkProduct )
-select gpu_descr, gpu_name, g3dm, g2dm, tdpw, brand, cprice, prevprice, stock, reviews, purl from rank_table where GPUrank = 1 order by gpu_name
-;
+select gpu_descr, gpu_name, g3dm, g2dm, tdpw, brand, cprice, prevprice, stock, reviews, purl from rank_table where GPUrank = 1 order by gpu_name;
 
-/* We notice from the query above we got a gpu_name 'ION' from the benchmark table, 
-this is not GPU is not in our neweggGPUDataUpdatedURL table
-so let's remove that as well from our results query */
+/* We notice from the query above we got a gpu_name 'ION' from the benchmark table, this is not GPU is not in our neweggGPUDataUpdatedURL table
+so let's remove that as well from our results query, let's also calculate the price to performace cost for each product for each benchmark 
+Price-to-Performance Cost = Benchmark / price https://www.videocardbenchmark.net/gpu_value.html
+high is better for price to performance*/
+
+/*this is what we will import into tableau*/
 with rank_table as (select *, rank() over(partition by gpu_descr order by gpu_descr, len(gpu_name) desc) as GPUrank from #joinedBenchmarkProduct )
-select gpu_descr, gpu_name, g3dm, g2dm, tdpw, brand, cprice, prevprice, stock, reviews from rank_table where GPUrank = 1 and gpu_name not like 'ion' order by gpu_name
+select gpu_descr as gpu_description, gpu_name, g3dm as G3DBenchmark, g2dm as G2DBenchmark, tdpw as TDP_W, brand, cprice as current_price, prevprice as previous_price, reviews,
+round((g3dm*1.0)/cprice, 3) as G3DBenchmarkPricetoPeform, round((g2dm*1.0)/cprice, 3) as G2DBenchmarkPricetoPeform
+from rank_table where GPUrank = 1 and gpu_name not like 'ion' order by gpu_name
 ;
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*I did all this work just to realize there is a function in tableau that can get the min and max price we want anyway so it isn't necessary*/
 /*now that we have that data let's insert into a temp table so we can further process some things before exporting it into tableau
-for example we would like to see the lowest and highest price of GPU for each category
-*/
+for example we would like to see the lowest and highest price of GPU for each category*/
 create table #finalResultTableProcess ( gpu_id int, gpu_category_id int,
 gpu_description varchar(255), gpu_name varchar(255), 
-g3dMark float, g2dMark float, tdpw nvarchar(255), brand varchar(255), CurrentPrice float, PreviousPrice float, stock varchar(255), reviews varchar(255) )
-;
-
+g3dMark float, g2dMark float, tdpw nvarchar(255), brand varchar(255), CurrentPrice float, PreviousPrice float, stock varchar(255), reviews varchar(255) );
 /*removes table for testing*/
 drop table #finalResultTableProcess;
-
 
 with rank_table as (select *, rank() over(partition by gpu_descr order by gpu_descr, len(gpu_name) desc) as GPUrank, 
 dense_rank() over (order by gpu_name) as gpu_category_id, 
 row_number() over(order by gpu_descr) as gpu_id from #joinedBenchmarkProduct )
 insert into #finalResultTableProcess
-select gpu_id, gpu_category_id, gpu_descr, gpu_name, g3dm, g2dm, tdpw, brand, cprice, prevprice, stock, reviews from rank_table where GPUrank = 1 and gpu_name not like 'ion' order by gpu_name
-;
+select gpu_id, gpu_category_id, gpu_descr, gpu_name, g3dm, g2dm, tdpw, brand, cprice, prevprice, stock, reviews from rank_table where GPUrank = 1 and gpu_name not like 'ion' order by gpu_name;
 
 select * from #finalResultTableProcess order by gpu_name;
 /*query that gives us the min and max price for each gpu_category*/
@@ -139,11 +143,7 @@ select gpu_category_id, gpu_name, min(currentPrice) as LowestGPUPrice, max(Curre
 
 select * from #finalResultTableProcess frtp inner join
 (select gpu_category_id, gpu_name, min(currentPrice) as LowestGPUPrice, max(CurrentPrice) as HighestGPUPrice from #finalResultTableProcess group by gpu_category_id, gpu_name) MaxMinGPUPrices
-on frtp.gpu_category_id = MaxMinGPUPrices.gpu_category_id
-;
-
-
-
+on frtp.gpu_category_id = MaxMinGPUPrices.gpu_category_id;
 
 /*this gives us the min and max price of ID for each gpu_name and also gives us the gpu_id, however, this information is not relevant for tableau purposes*/
 with cte_priceRank as(
